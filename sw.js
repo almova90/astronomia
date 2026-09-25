8398e74e0e738586b34e6b86d55473ff642ad043
Álvaro Moreno-Vallori <58228562+almova90@users.noreply.github.com>
2026-09-25 15:46:15 +0200

    Actualizar caché de iconos en Astronomía v0.1.3

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const APP_VERSION='0.1.2';
+const APP_VERSION='0.1.3';
 const CACHE_NAME=`astronomia-${APP_VERSION}`;
 const APP_SHELL=[
   './',
   './index.html',
   './manifest.webmanifest',
-  './icon.svg'
+  './icon.svg',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install',event=>{

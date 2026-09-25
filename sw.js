75341a4a9074f28aaa32cd326ff08cbd0b14b992
Álvaro Moreno-Vallori <58228562+almova90@users.noreply.github.com>
2026-09-25 16:26:40 +0200

    Reparar caché PWA v0.1.5

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const APP_VERSION='0.1.4';
+const APP_VERSION='0.1.5';
 const CACHE_NAME=`astronomia-${APP_VERSION}`;
 const APP_SHELL=[
   './',
   './index.html',
   './manifest.webmanifest',
   './icon.svg',
-  './icon-192-v014.png',
-  './icon-512-v014.png'
+  './icon-192.png',
+  './icon-512.png',
 ];
 
 self.addEventListener('install',event=>{
